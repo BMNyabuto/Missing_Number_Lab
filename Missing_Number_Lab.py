26da9fe9cfbc6273121f5a562ca5b0c64b61af2b
boman <bmnyabuto@gmail.com>
2017-01-19 14:26:37 +0300

    Added a statement that converts arrays into sets and subtracts them to remain with a list containing the distinct element; and also added if statement that checks whether the resulting list is an empty

--- Missing_Number_Lab.py
+++ Missing_Number_Lab.py
@@ -1,3 +1,6 @@
 def find_missing(list1, list2):
 	if isinstance(list1, list) == True:
             if isinstance(list2, list) == True:
+            	
+            	ListDiff_1 = list(set(list1)-set(list2))
+            	if ListDiff_1 == []:
