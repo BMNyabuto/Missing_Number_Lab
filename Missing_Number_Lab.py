62bf2ad8c93ef4096bf8552d0276dade79b52237
boman <bmnyabuto@gmail.com>
2017-01-19 14:35:00 +0300

    Added a statement that runs if the latter condition is passed, whereby it transposes the arrays and counterchecks the latter result

--- Missing_Number_Lab.py
+++ Missing_Number_Lab.py
@@ -1,6 +1,7 @@
 def find_missing(list1, list2):
 	if isinstance(list1, list) == True:
             if isinstance(list2, list) == True:
-            	
+
             	ListDiff_1 = list(set(list1)-set(list2))
             	if ListDiff_1 == []:
+            		ListDiff_2 = list(set(list2)-set(list1))
